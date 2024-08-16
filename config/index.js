015a42a40550ca91963cbac606323c5274014739
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-08-16 08:16:12 -0700

    Remove Google API Key

--- config/index.js
+++ config/index.js
@@ -1,7 +1,4 @@
 const Config = {
-  RECAPTCHA_SITE_KEY: '6Lfk-ScqAAAAAKWcznfXnRneskhYQalzvQGGYyHT',
-  GOOGLE_API_KEY: 'AIzaSyAdI4ZiOALebJoDBtBLBheNp9L7MEUl1F4',
-
   BASE_URL: 'http://localhost:3000',
   VERIFY_EMAIL_TIME: '3h',
   ACCESS_TOKEN_TIME: '5h',
