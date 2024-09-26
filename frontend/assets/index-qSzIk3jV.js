aefdf2e80553387aaa91c1ea9571c53c3528cdca
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-09-26 03:51:34 -0500

    update recatcha key

---
+++ frontend/assets/index-qSzIk3jV.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-DKQ0p290.js";import{S as i,g as x,a5 as m}from"./App-KPJk7nGf.js";import{T as l}from"./images-D6HjK07w.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
