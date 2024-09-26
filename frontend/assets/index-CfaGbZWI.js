439aae9091d3f43a6729b6b75f540f40c2e5c617
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-09-25 23:26:37 -0500

    update API URL

---
+++ frontend/assets/index-CfaGbZWI.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-HroY3NYC.js";import{S as i,g as x,a5 as m}from"./App-Bx55w4lY.js";import{T as l}from"./images-cpzznJFS.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
