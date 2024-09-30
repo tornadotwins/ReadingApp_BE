e28e51ea53c8dcff5e2b88ecd03375347965b70c
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-09-30 03:42:59 -0500

    kan-2/update reset (frontend)
    Updated reset page in order to align form controllers to vertical center

--- frontend/assets/index-BN-eltte.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-CDMXNg3a.js";import{S as i,g as x,a6 as m}from"./App-Dsa24REC.js";import{T as l}from"./images-DYrpb_iJ.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
