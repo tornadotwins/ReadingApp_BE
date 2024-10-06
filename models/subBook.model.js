7aaf712ec04c53bc9589b854a5dda6371f4b74de
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-10-06 07:25:37 -0500

    add noChapter field

--- models/subBook.model.js
+++ models/subBook.model.js
@@ -7,6 +7,7 @@ const SubBookSchema = new Schema({
   number: { type: Number },
   title: { type: Object },
   book: { type: Schema.Types.ObjectId, ref: 'Book' },
+  noChapter: { type: Boolean, default: false },
   createdAt: { type: Number, default: Date.now() },
   updatedAt: { type: Number, default: Date.now() },
 });
