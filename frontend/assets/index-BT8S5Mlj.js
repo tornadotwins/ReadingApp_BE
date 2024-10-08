40608ad8cb4fc81f4ad33b255d7be515b6075e28
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-10-08 08:34:59 -0500

    changed config for hosted envs

--- frontend/assets/index-BT8S5Mlj.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-oYoQThXz.js";import{S as i,g as x,a6 as m}from"./App-Dt7VHeJa.js";import{T as l}from"./images-B5tPiil3.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
---
+++ frontend/assets/index-DHoCgMPN.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-Dibo9daf.js";import{S as i,g as x,a6 as m}from"./App-DcjgFMkt.js";import{T as l}from"./images-CNt9VUNy.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
