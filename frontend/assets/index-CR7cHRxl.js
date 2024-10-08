219775afbbf44d51e0bae5f7ad5a174d55c6472c
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-10-08 18:06:15 -0500

    fix/responsive _ UI

--- frontend/assets/index-CR7cHRxl.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-Dibo9daf.js";import{S as i,g as x,a6 as m}from"./App-D9pPCGD4.js";import{T as l}from"./images-D3BSKDzw.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
