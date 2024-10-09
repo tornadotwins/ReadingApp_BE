2fce460eba4e418dc82f004899e8c75495d5c25d
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-10-09 17:01:47 -0500

    fix/undefined url in UI

--- frontend/assets/index-Du5qWkO4.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-Dibo9daf.js";import{S as i,g as x,a6 as m}from"./App-CRnGzRhp.js";import{T as l}from"./images-DZji_gVo.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
