884c83a558a2d820ab0e324d78237365a476d369
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-10-10 13:04:23 -0500

    fix self-QA issues

---
+++ frontend/assets/index-CrILDL9r.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-Dibo9daf.js";import{S as i,g as x,a6 as m}from"./App-OdTok9eR.js";import{T as l}from"./images-DnnUPU9h.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
