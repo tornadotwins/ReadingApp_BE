71a85b1d55ddcc0309d8dc9568758882d6ba5e40
mfchb094 <147813832+livewizard93@users.noreply.github.com>
2024-10-14 09:57:35 -0500

    fix/ accordion moving

---
+++ frontend/assets/index-B1_ruNJh.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-TfMfeDo7.js";import{S as i,g as x,Z as m}from"./App-BORvBstc.js";import{T as l}from"./images-BSDyU14y.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
