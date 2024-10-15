456610f8bd8ccf5dcc4af435b6570e4577d4eb29
mfchb094 <147813832+livewizard93@users.noreply.github.com>
2024-10-15 09:09:51 -0500

    fix some issues in UI
    - hide verse number in intro page
    - show display intro verses in verse_by_verse mode
    - reset all redux store when reopening app

---
+++ frontend/assets/index-bZcbw_Tg.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-DDg5x1vp.js";import{S as i,g as x,Z as m}from"./App-BWJORKtV.js";import{T as l}from"./images-BVMc-T4x.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
