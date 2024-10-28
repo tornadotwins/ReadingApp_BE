b0201cb9fa1c7a68621e4b41f6d1d3ba737e7c4d
livewizard93 <fengbeststar@gmail.com>
2024-10-28 12:59:27 -0500

    fix url of admin

--- views/src/config/index.ts
+++ views/src/config/index.ts
@@ -2,7 +2,8 @@ import isMobile from '@/utils/is-mobile';
 
 import type { Notifications } from './types';
 
-const API_URL = 'http://localhost:5000';
+// const API_URL = 'http://localhost:5000';
+const API_URL = 'https://readingapp-d5944320053c.herokuapp.com';
 
 const title = 'React PWA';
 
