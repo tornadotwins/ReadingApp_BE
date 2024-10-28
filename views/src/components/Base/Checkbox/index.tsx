2d52417830e7542be6dbb9979e7715603f34d44c
livewizard93 <fengbeststar@gmail.com>
2024-10-28 10:23:23 -0500

    fix/edit checkbox

--- views/src/components/Base/Checkbox/index.tsx
+++ views/src/components/Base/Checkbox/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   StyledCheckBoxContainer,
   StyledCheckBoxLabel,
@@ -11,14 +11,20 @@ import Text from '../Text';
 import Images from '@/config/images';
 
 function Checkbox(props: CheckboxProps) {
-  // State to keep track of whether the checkbox is checked
+  // Remove local state and use props directly
   const [isChecked, setIsChecked] = useState(props.checked || false);
+
+  // Add useEffect to sync with prop changes
+  useEffect(() => {
+    setIsChecked(props.checked || false);
+  }, [props.checked]);
 
   // Handle checkbox click
   const handleCheckboxChange = () => {
-    setIsChecked(!isChecked); // Toggle the checked state
+    const newValue = !isChecked;
+    setIsChecked(newValue);
     if (props.onChange) {
-      props.onChange(!isChecked);
+      props.onChange(newValue);
     }
   };
 
@@ -26,13 +32,21 @@ function Checkbox(props: CheckboxProps) {
     <StyledCheckBoxContainer>
       {props.label && (
         <StyledCheckBoxLabel>
-          <Text fontFamily='"Baloo Da 2"' fontSize={16} lineHeight={24} textAlign='center'>
+          <Text
+            fontFamily='"Baloo Da 2"'
+            fontSize={16}
+            lineHeight={24}
+            textAlign='center'
+          >
             {props.label}
           </Text>
         </StyledCheckBoxLabel>
       )}
 
-      <StyledCheckBox onClick={handleCheckboxChange}>
+      <StyledCheckBox
+        onClick={handleCheckboxChange}
+        ref={props.reference}
+      >
         <StyledCheckBoxImgContainer>
           {isChecked && <StyledCheckBoxImg src={Images.icon_check} />}
         </StyledCheckBoxImgContainer>
