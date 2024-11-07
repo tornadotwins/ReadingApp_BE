41a2b3b364045b0d3bfcb3a76689f337728f34f1
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-11-06 19:42:25 -0500

    fix/KAN-15
    With verse-per-language option the app bugs on all pages and shows an “or” box

--- frontend/assets/index-2rFVQD2q.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-DDg5x1vp.js";import{S as i,g as x,Y as m}from"./App-D1G5kpsm.js";import{T as l}from"./images-B-Cl5WlU.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
