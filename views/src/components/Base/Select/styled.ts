aaee30ac278c3090c179c117e3a61667e94ea7df
livewizard93 <fengbeststar@gmail.com>
2024-11-21 11:42:30 -0500

    disable scroll lock when open dropdown

--- views/src/components/Base/Select/styled.ts
+++ views/src/components/Base/Select/styled.ts
@@ -66,6 +66,7 @@ const StyledMenuItem = styled(MuiMenuItem)<{dividelinecolor: string}>(({ selecte
 
 // Custom styles for the Paper component
 const MenuProps = {
+  disableScrollLock: true,
   PaperProps: {
     sx: {
       bgcolor: '#C0C0C0',
