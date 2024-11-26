0f36629769d25e4e74d3b6d75eb66921eb40968d
livewizard93 <fengbeststar@gmail.com>
2024-11-26 12:53:00 -0500

    updated config URL

---
+++ admin/assets/index-DhZPArjs.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-C7nUuxX_.js";import{K as r,aI as a,T as t}from"./App-B9rHvszB.js";import"./index-DV8uR7_z.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"page 3"}),e.jsx(a,{children:e.jsx(t,{variant:"h3",children:"Page 3"})})]})}export{o as default};
