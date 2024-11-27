6f36e59a90a60c106bdff920be5ef2ada2e129be
livewizard93 <fengbeststar@gmail.com>
2024-11-27 13:13:54 -0500

    updated ui for admin

---
+++ admin/assets/index-BeXr8C2n.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-DG-pJtVH.js";import{K as r,aI as a,T as t}from"./App-CInRyRO6.js";import"./index-B_DB3_MX.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(r,{title:"page 3"}),e.jsx(a,{children:e.jsx(t,{variant:"h3",children:"Page 3"})})]})}export{o as default};
--- admin/assets/index-Dbw70rJV.js
+++
@@ -1,1 +0,0 @@
-import{L as r}from"./App-C2Yz7aTm.js";import"./createTheme-DJPBJvZP.js";import"./index-B2VUIFA_.js";export{r as default};
