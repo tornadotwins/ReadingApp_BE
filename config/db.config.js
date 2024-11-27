6ef0eb771fe2186cbfbba88e46b63e75ee06e398
livewizard93 <fengbeststar@gmail.com>
2024-11-27 01:08:33 -0500

    built api for app translator
    - get all app texts
    - create app text page
    - create app text
    - update app texts
    - update app text page

--- config/db.config.js
+++ config/db.config.js
@@ -1,5 +1,5 @@
-const url = 'mongodb+srv://mfchb094:GGGQbosUP2pT7Ln6@readningapp.5sllf.mongodb.net/readingapp?retryWrites=true&w=majority&appName=ReadningApp';
-// const url = 'mongodb://localhost:27017';
+// const url = 'mongodb+srv://mfchb094:GGGQbosUP2pT7Ln6@readningapp.5sllf.mongodb.net/readingapp?retryWrites=true&w=majority&appName=ReadningApp';
+const url = 'mongodb://localhost:27017';
 
 module.exports = {
   url
--- routes/translate.route.js
+++ routes/translate.route.js
@@ -4,7 +4,10 @@ const { checkToken } = require('../utils');
 
 const router = express.Router();
 
+router.get('/appTexts', TranslatorController.getAppTexts);
 router.post('/createPage', TranslatorController.createAppTextPage);
 router.post('/createAppText', TranslatorController.createAppText);
+router.put('/appTexts', TranslatorController.updateAppTexts);
+router.put('/appTextPage', TranslatorController.updateAppTextPage);
 
 module.exports = router;
