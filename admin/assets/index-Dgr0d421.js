0a8ecee20cbfc0771f751ae6fe465ebeec7b1f0a
livewizard93 <fengbeststar@gmail.com>
2024-11-28 02:35:42 -0500

    disable isPublish without isComplete

---
+++ admin/assets/index-Dgr0d421.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-Gc-mA0LN.js";import{M as a,aI as r,ah as t}from"./App-BEBoIvYs.js";import"./index-nWCbLmQJ.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"page 2"}),e.jsx(r,{children:e.jsx(t,{variant:"h3",children:"Page 2"})})]})}export{o as default};
---
+++ admin/assets/index-gDskwIcG.js
@@ -0,0 +1,1 @@
+import{L as r}from"./App-BEBoIvYs.js";import"./createTheme-Gc-mA0LN.js";import"./index-nWCbLmQJ.js";export{r as default};
