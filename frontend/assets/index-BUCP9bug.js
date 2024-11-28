7aa75089a042b091e08a51582e0b8870993213c5
livewizard93 <fengbeststar@gmail.com>
2024-11-28 07:47:20 -0500

    fixed ReadingApp

---
+++ frontend/assets/index-BUCP9bug.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BPBmJKVk.js";import{aQ as t,aR as o}from"./App-CPmoLqX-.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
