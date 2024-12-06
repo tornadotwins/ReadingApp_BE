ec11716470d801a50bd7bf7878512b0907782cdf
livewizard93 <fengbeststar@gmail.com>
2024-12-06 03:25:56 -0500

    SubBookExplorer

--- admin/assets/index-Cb4o3jcL.js
+++
@@ -1,1 +0,0 @@
-import{L as r}from"./App-Bdw6BpTK.js";import"./createTheme-BALI1a3O.js";import"./index-EvO0Tsir.js";export{r as default};
---
+++ frontend/assets/index-BCk2EnXO.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Co3tk9z7.js";import{aT as t,aU as o}from"./App-MSpuw0Ml.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
