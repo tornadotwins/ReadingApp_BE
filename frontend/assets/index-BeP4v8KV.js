fa1f2ba0eb5ca4998faf1835f031395e8c93011b
livewizard93 <fengbeststar@gmail.com>
2024-12-09 04:30:45 -0500

    jump to verse

---
+++ frontend/assets/index-BeP4v8KV.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-D44CcyU6.js";import{S as i,g as x,Y as m}from"./App-U1tkjY3a.js";import{T as l}from"./index-Chl_fzlN.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
---
+++ frontend/assets/index-DWHEEoGF.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D44CcyU6.js";import{aT as t,aU as o}from"./App-U1tkjY3a.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
