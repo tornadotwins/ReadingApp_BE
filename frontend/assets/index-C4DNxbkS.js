3cef097b0d1e049f2381b114ec5bd2100cefc07e
livewizard93 <fengbeststar@gmail.com>
2024-12-09 02:39:44 -0500

    fixed some issues for Reading page

---
+++ frontend/assets/index-C4DNxbkS.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BPBmJKVk.js";import{aT as t,aU as o}from"./App-UZonZDcL.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
---
+++ frontend/assets/index-D689cL0m.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Co3tk9z7.js";import{aT as t,aU as o}from"./App-DLH-nwlH.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
