d409e522267fb4b00b84d71dcd08501fb6ba6cab
livewizard93 <fengbeststar@gmail.com>
2024-12-09 12:45:42 -0500

    fix some issues
    1. Scroll to top when prev/next
    2. remove scrolling when navigating from Library page

--- frontend/assets/index-CtnP4wfx.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-BPBmJKVk.js";import{S as i,g as x,Y as m}from"./App-Dev0GCGR.js";import{T as l}from"./index-DlIwGATM.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
---
+++ frontend/assets/index-Dyu6berz.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-BPBmJKVk.js";import{S as i,g as x,Y as m}from"./App-Daioa5OK.js";import{T as l}from"./index-prtcgOaC.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
