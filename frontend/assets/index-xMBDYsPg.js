59d9cee8ed0092a44b8620165982717a3031b0e5
livewizard93 <fengbeststar@gmail.com>
2024-12-09 11:55:54 -0500

    scroll to specific sub book

---
+++ frontend/assets/index-xMBDYsPg.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-BPBmJKVk.js";import{S as i,g as x,Y as m}from"./App-Dod54FHb.js";import{T as l}from"./index-uV_Y_vZg.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
