4db53aeac7f07fbadf98e4ce4877663da7d7ee22
livewizard93 <fengbeststar@gmail.com>
2024-12-09 08:05:22 -0500

    set chapter & verse as 1 in SubBookExplorer

---
+++ frontend/assets/index-BYryFmP0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BPBmJKVk.js";import{aT as t,aU as o}from"./App-F7j2TeW5.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
