d1ac879487c3dd7b263b3d86394c6fa87586b8a0
livewizard93 <fengbeststar@gmail.com>
2024-12-10 07:48:35 -0500

    complete showing title

---
+++ frontend/assets/index-CMIT00nS.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-CJuk4MNS.js";import{S as i,g as x,Y as m}from"./App-DeRgSr8m.js";import{T as l}from"./index-BbrulkAu.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
