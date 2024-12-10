7dd3b02d7e9dbbedaedc97f4a85b9ee5aaa483c9
livewizard93 <fengbeststar@gmail.com>
2024-12-10 08:14:33 -0500

    scroll to opened sub book

---
+++ frontend/assets/index-CAuUpbUn.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-CJuk4MNS.js";import{S as i,g as x,Y as m}from"./App-DBVpnZNp.js";import{T as l}from"./index-3mjEJHbU.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
