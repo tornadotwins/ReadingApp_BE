7060af09d9b025b6a5b59829090933077b07a53d
livewizard93 <fengbeststar@gmail.com>
2024-12-10 07:19:56 -0500

    complete displaying title

---
+++ frontend/assets/index-ULb8CiS7.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CJuk4MNS.js";import{aT as t,aU as o}from"./App-DdatzoQ7.js";function i(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{i as L};
