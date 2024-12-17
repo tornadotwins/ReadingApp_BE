3133bb86008936d76f6c1c8e61fd845e884263ab
livewizard93 <fengbeststar@gmail.com>
2024-12-17 15:26:57 -0500

    start point highlight

--- views/src/components/TimeLine/styles.ts
+++ views/src/components/TimeLine/styles.ts
@@ -8,16 +8,17 @@ const StyledTimeLineProgressContainer = styled(Box)({
   boxSizing: 'border-box',
   margin: '20px 40px',
   cursor: 'pointer',
-  position: 'relative'
+  position: 'relative',
 });
 
 const StyledRedBar = styled(Box)<{ isactive: string }>(({isactive}) => ({
   position: 'absolute',
   width: '5px', // default width for the red bar
-  height: '100%', // Full height of the container
+  height: isactive == 'true' ? '20px' : '10px', // Full height of the container
   backgroundColor: isactive == 'true' ? 'orange' : 'red', // Change color when active
   boxShadow: isactive == 'true' ? '0 0 10px 2px rgba(255, 165, 0, 0.5)' : 'none', // Highlight effect when active
   transition: 'background-color 0.3s ease, box-shadow 0.3s ease', // Smooth transition effect
+  bottom: isactive == 'true' ? '-5px' : '0px'
 }));
 
 export {
