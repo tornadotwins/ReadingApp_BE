4d49dc8323a71f2cfd1c8f3ca12108c612f74ce4
livewizard93 <fengbeststar@gmail.com>
2024-12-17 23:14:47 -0500

    removed lock file

---
+++ admin/assets/index-CHihdlTX.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-BYbBl0_b.js";import{M as a,aQ as r,a9 as t}from"./App-6jtlpKvU.js";import"./index-BO_joa27.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"page 2"}),e.jsx(r,{children:e.jsx(t,{variant:"h3",children:"Page 2"})})]})}export{o as default};
