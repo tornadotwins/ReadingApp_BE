de410e7ede6256103a96b78adda00d532c0132c1
livewizard93 <fengbeststar@gmail.com>
2024-12-22 13:17:25 -0500

    intro uploader

---
+++ admin/assets/index-BiY8wA53.js
@@ -0,0 +1,1 @@
+import{L as r}from"./App-D_6gwKuw.js";import"./createTheme-BuP8AzxQ.js";import"./index-C6lco80w.js";export{r as default};
---
+++ admin/assets/index-DyotRKoS.js
@@ -0,0 +1,1 @@
+import{a as r,j as t}from"./createTheme-BuP8AzxQ.js";import{aG as o,aH as c}from"./App-D_6gwKuw.js";function x(i){const[a,n]=r.useState(null),l=e=>{const s=e.target.files?e.target.files[0]:null;i.onChange(e),n(s)};return t.jsxs(o,{children:[t.jsx(c,{type:"file",onChange:l,inputProps:{accept:i.type},variant:"outlined"}),a&&t.jsx("p",{children:a.name})]})}export{x as F};
