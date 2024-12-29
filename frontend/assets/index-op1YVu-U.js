9baa3d01e255ec8da0cddb4193f79c333d93a83b
winterbreaker23 <147813832+livewizard93@users.noreply.github.com>
2024-12-29 17:38:34 -0500

    remove cache

---
+++ frontend/assets/index-op1YVu-U.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-H5ykdk14.js";import{S as i,g as x,Y as m}from"./App-DQQ9GWEY.js";import{T as l}from"./index-CSvUHeQw.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
