42a0fc9e0bf07015ba0621e1fd46ba58a9c487ac
livewizard93 <fengbeststar@gmail.com>
2025-01-24 01:50:03 -0500

    Journey

---
+++ frontend/assets/useOrientation-Dh3yhf8T.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-BlVR3GtE.js";function n(){return window.innerHeight>window.innerWidth}function s(){const[i,r]=t.useState(n());return t.useEffect(()=>{function e(){r(n())}return window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[]),i}export{s as u};
