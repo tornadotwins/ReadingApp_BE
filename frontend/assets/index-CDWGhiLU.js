ba2080d8880196f7e70e4bd9517196fdad6f9c83
livewizard93 <fengbeststar@gmail.com>
2025-01-27 09:35:09 -0500

    updated journey reading page

--- frontend/assets/index-CDWGhiLU.js
+++
@@ -1,1 +0,0 @@
-import{b as a,j as t}from"./index-BlVR3GtE.js";import{S as i,g as x,_ as m}from"./App-BwzROJtC.js";import{T as l}from"./index-T3aZZFzM.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
