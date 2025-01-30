c40e324d878dfadf8251e7c4209e4d0dd5fd44ee
livewizard93 <fengbeststar@gmail.com>
2025-01-30 12:38:16 -0500

    updated admin ui

---
+++ admin/assets/index-D1w0D0_S.js
@@ -0,0 +1,1 @@
+import{a as r,j as t}from"./createTheme-fJ5dSSTW.js";import{av as o,aw as c}from"./App-CaXAlDue.js";function x(i){const[a,n]=r.useState(null),l=e=>{const s=e.target.files?e.target.files[0]:null;i.onChange(e),n(s)};return t.jsxs(o,{children:[t.jsx(c,{type:"file",onChange:l,inputProps:{accept:i.type},variant:"outlined"}),a&&t.jsx("p",{children:a.name})]})}export{x as F};
