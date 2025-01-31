a2e1c2de404698c67346ebeb0d7779160fef1ced
livewizard93 <fengbeststar@gmail.com>
2025-01-31 06:24:34 -0500

    updated Admin UI

---
+++ admin/assets/index-DBOkcGRY.js
@@ -0,0 +1,1 @@
+import{L as r}from"./App-C-IcCATk.js";import"./createTheme-DLEF8iBv.js";import"./index-DR07mdKK.js";export{r as default};
---
+++ admin/assets/index-DraJqV0_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-DLEF8iBv.js";import{M as a,aV as r,ad as t}from"./App-C-IcCATk.js";import"./index-DR07mdKK.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"page 2"}),e.jsx(r,{children:e.jsx(t,{variant:"h3",children:"Page 2"})})]})}export{o as default};
