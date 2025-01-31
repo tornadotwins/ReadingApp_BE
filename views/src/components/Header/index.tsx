92f228acc3c7550f6db3e1a51ffd18342854e82a
livewizard93 <fengbeststar@gmail.com>
2025-01-30 22:06:21 -0500

    hide header texts in login page

--- views/src/components/Header/index.tsx
+++ views/src/components/Header/index.tsx
@@ -14,59 +14,64 @@ function Header(props: HeaderType) {
 
   return (
     <StyledHeaderContainer>
-      <StyledHeaderTitle>
-        {
-          props.isAdmin &&
-          <Text
-            fontFamily="Buenard"
-            fontWeight="700"
-            fontSize={36}
-            lineHeight={47}
-            textAlign="center"
-            color="white"
-            hasUnderline={props.isAdminPage}
-            onClick={() => navigate('/admin/admin-portal')}
-          >
-            {'Admin Portal'}
-          </Text>
-        }
-        <Text
-          fontFamily="Buenard"
-          fontWeight="700"
-          fontSize={36}
-          lineHeight={47}
-          textAlign="center"
-          color="white"
-          hasUnderline={props.isTranslationPage}
-          onClick={() => navigate('/admin/bookoverview')}
-        >
-          {'Translation Portal'}
-        </Text>
-        <Text
-          fontFamily="Buenard"
-          fontWeight="700"
-          fontSize={36}
-          lineHeight={47}
-          textAlign="center"
-          color="white"
-          hasUnderline={!props.isAdminPage && props.isTranslationPage}
-          onClick={() => navigate('/admin/journeyoverview')}
-        >
-          {'Journey Portal'}
-        </Text>
-      </StyledHeaderTitle>
-
       {
-        props.isLoggedIn && props.username && props.onLogOut &&
-        <StyledNavbar>
-          <StyledUserName>
-            <Text fontFamily="'Baloo Da 2'" fontWeight="700" fontSize={16} lineHeight={24} color="white">
-              {props.username}
+        props.isLoggedIn &&
+        <>
+          <StyledHeaderTitle>
+            {
+              props.isAdmin &&
+              <Text
+                fontFamily="Buenard"
+                fontWeight="700"
+                fontSize={36}
+                lineHeight={47}
+                textAlign="center"
+                color="white"
+                hasUnderline={props.isAdminPage}
+                onClick={() => navigate('/admin/admin-portal')}
+              >
+                {'Admin Portal'}
+              </Text>
+            }
+            <Text
+              fontFamily="Buenard"
+              fontWeight="700"
+              fontSize={36}
+              lineHeight={47}
+              textAlign="center"
+              color="white"
+              hasUnderline={props.isTranslationPage}
+              onClick={() => navigate('/admin/bookoverview')}
+            >
+              {'Translation Portal'}
             </Text>
-          </StyledUserName>
+            <Text
+              fontFamily="Buenard"
+              fontWeight="700"
+              fontSize={36}
+              lineHeight={47}
+              textAlign="center"
+              color="white"
+              hasUnderline={!props.isAdminPage && props.isTranslationPage}
+              onClick={() => navigate('/admin/journeyoverview')}
+            >
+              {'Journey Portal'}
+            </Text>
+          </StyledHeaderTitle>
 
-          <Button label={'Log Out'} onClick={props.onLogOut} />
-        </StyledNavbar>
+          {
+            props.isLoggedIn && props.username && props.onLogOut &&
+            <StyledNavbar>
+              <StyledUserName>
+                <Text fontFamily="'Baloo Da 2'" fontWeight="700" fontSize={16} lineHeight={24} color="white">
+                  {props.username}
+                </Text>
+              </StyledUserName>
+
+              <Button label={'Log Out'} onClick={props.onLogOut} />
+            </StyledNavbar>
+          }
+        </>
       }
     </StyledHeaderContainer>
   )
