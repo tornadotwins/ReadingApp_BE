94cd0066c163c929bc5a13070f3225bc762b45d3
livewizard93 <fengbeststar@gmail.com>
2025-02-10 06:35:30 -0500

    Update lake.controller.js

--- controllers/lake.controller.js
+++ controllers/lake.controller.js
@@ -4,20 +4,14 @@ const axios = require('axios');
 ////////////////////////////// Save Article /////////////////////////////
 /////////////////////////////////////////////////////////////////////////
 exports.getRoad = async (req, res) => {
-  try {
-    axios
-      .get(
-        'https://trafficnz.info/service/traffic/rest/4/events/withinbounds/175.2352/-39.5744/176.5243/-38.1838/10',
-      )
-      .then((response) => {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        res.status(500).json(error);
-      });
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-  }
+  axios
+    .get(
+      'https://trafficnz.info/service/traffic/rest/4/events/withinbounds/175.2352/-39.5744/176.5243/-38.1838/10',
+    )
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 };
