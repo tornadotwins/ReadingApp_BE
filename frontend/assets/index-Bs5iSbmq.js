f4869e88274f496bf30df1c2dac85d4219509867
livewizard93 <fengbeststar@gmail.com>
2025-02-20 22:54:55 -0500

    Updated frontend version

---
+++ frontend/assets/index-Bs5iSbmq.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CwXo1Pyr.js";import{b1 as t,b2 as o}from"./App-CZTJOJAz.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
