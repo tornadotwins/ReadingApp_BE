4ec86b6726054049c8e0f44c219faed611118167
livewizard93 <fengbeststar@gmail.com>
2025-02-21 01:19:52 -0500

    Update Reading Language from Settings page

---
+++ frontend/assets/index-ChAK6QqK.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DC16-UlT.js";import{b1 as t,b2 as o}from"./App-B6IVJFDP.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
