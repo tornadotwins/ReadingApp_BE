e7e09da8a7f89374901edd427123442f31e44a3d
livewizard93 <fengbeststar@gmail.com>
2025-02-26 10:32:10 -0500

    QA bug fix

---
+++ admin/assets/index-BRzy-wVs.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-CsEn8Xwk.js";import{M as a,aV as r,ad as t}from"./App-BKacFRzX.js";import"./index-BSm4_Lil.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"page 3"}),e.jsx(r,{children:e.jsx(t,{variant:"h3",children:"Page 3"})})]})}export{o as default};
---
+++ admin/assets/index-JJVHcBUm.js
@@ -0,0 +1,1 @@
+import{L as r}from"./App-BKacFRzX.js";import"./createTheme-CsEn8Xwk.js";import"./index-BSm4_Lil.js";export{r as default};
