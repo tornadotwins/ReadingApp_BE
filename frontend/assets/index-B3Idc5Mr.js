625a42af64af9a36aa9296c401e2a96e2361192c
livewizard93 <fengbeststar@gmail.com>
2025-03-01 18:00:48 -0500

    changed app version

---
+++ frontend/assets/index-B3Idc5Mr.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BtHY-KAx.js";import{b1 as t,b2 as o}from"./App-B1-GqC3j.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
---
+++ frontend/assets/index-CtOH4idG.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-BtHY-KAx.js";import{S as i,g as x,a7 as m}from"./App-B1-GqC3j.js";import{T as l}from"./index-v9R8iKio.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
