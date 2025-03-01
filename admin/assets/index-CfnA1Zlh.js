a3be727506ca52c9155c4eabc25d11b85ed6a069
livewizard93 <fengbeststar@gmail.com>
2025-03-01 14:20:01 -0500

    Fixed issues in admin

---
+++ admin/assets/index-CfnA1Zlh.js
@@ -0,0 +1,1 @@
+import{j as e}from"./createTheme-CsEn8Xwk.js";import{M as a,aV as r,ad as t}from"./App-BKacFRzX.js";import"./index-BSm4_Lil.js";function o(){return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"page 2"}),e.jsx(r,{children:e.jsx(t,{variant:"h3",children:"Page 2"})})]})}export{o as default};
