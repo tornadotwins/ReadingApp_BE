cd0c781c3a3f565c629407d7a7198c59217afb0e
livewizard93 <fengbeststar@gmail.com>
2025-03-19 18:44:40 -0500

    show en psalm in unknown language

---
+++ frontend/assets/ArrowBack-B4E3Pcc-.js
@@ -0,0 +1,1 @@
+import{X as r,j as a}from"./index-DEG7EQ5W.js";import{r as t}from"./App-B80AJ_DC.js";var e={},o=r;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,i=o(t()),l=a;u=e.default=(0,i.default)((0,l.jsx)("path",{d:"M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20z"}),"ArrowBack");export{u as d};
