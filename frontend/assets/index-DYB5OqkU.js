16e9b2c520c99584d455131776f242f8a540672b
livewizard93 <fengbeststar@gmail.com>
2025-03-19 02:35:08 -0500

    scroll to title when open reading page

---
+++ frontend/assets/index-DYB5OqkU.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-a5M0-iHg.js";import{S as i,g as x,a9 as m}from"./App-DZI9H0KI.js";import{T as l}from"./index-BhXN5f6v.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
