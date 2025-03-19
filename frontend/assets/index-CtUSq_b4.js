5dfa9397e13932732eedc7f2e4b6058e22d2e053
livewizard93 <fengbeststar@gmail.com>
2025-03-18 21:59:57 -0500

    fix scrolling to title in white panel

---
+++ frontend/assets/index-CtUSq_b4.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-a5M0-iHg.js";import{b5 as t,b6 as o}from"./App-CVSM3Vok.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
