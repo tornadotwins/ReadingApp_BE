22595bf5fbb79b010d2e0154371406d271f26712
livewizard93 <fengbeststar@gmail.com>
2025-03-18 22:55:24 -0500

    scroll to top when open new chapter

---
+++ frontend/assets/index-DHqSrV-i.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-a5M0-iHg.js";import{S as i,g as x,a9 as m}from"./App-C4kJ5i2U.js";import{T as l}from"./index-CLnXJqJj.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
