f08d30bc3b1b5ed3a4a5ed48925230f654642840
livewizard93 <fengbeststar@gmail.com>
2025-03-19 20:44:00 -0500

    fix position of book title and search image

---
+++ frontend/assets/index-6B6S6VaG.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-DEG7EQ5W.js";import{S as i,g as x,a9 as m}from"./App-DgKGe4wk.js";import{T as l}from"./index-YIZxQGig.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
---
+++ frontend/assets/index-DBFuRob-.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-DEG7EQ5W.js";import{b5 as t,b6 as o}from"./App-DgKGe4wk.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
