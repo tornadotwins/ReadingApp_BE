1b7a9801b6cc91c050ee9bc4bdd70f2c57689d5f
livewizard93 <fengbeststar@gmail.com>
2025-03-27 02:54:49 -0500

    fixed issues on app _ 20250327

---
+++ frontend/assets/index-Dd4gRlHj.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-vVnpQR20.js";import{S as i,g as x,a9 as m}from"./App-D3CD6LKw.js";import{T as l}from"./index-DjLmZrKi.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
---
+++ frontend/assets/index-KcwUpDEO.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-vVnpQR20.js";import{b5 as t,b6 as o}from"./App-D3CD6LKw.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
