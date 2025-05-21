67801f8d295b27db5e9512d91583cdf5a1734cff
livewatter009 <fengbeststar@gmail.com>
2025-05-21 05:16:12 -0500

    Fix issues
    1. Hide chapter number 0 in Chapter Translation portal
    2. Hide the unpublished verses in reader side

---
+++ frontend/assets/index-yWo53PhY.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-D_Xdl7oi.js";import{b5 as t,b6 as o}from"./App-CJCErJ-_.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
---
+++ frontend/assets/useOrientation-OJQVjEOK.js
@@ -0,0 +1,1 @@
+import{b as t}from"./index-D_Xdl7oi.js";function n(){return window.innerHeight>window.innerWidth}function s(){const[i,r]=t.useState(n());return t.useEffect(()=>{function e(){r(n())}return window.addEventListener("resize",e),()=>window.removeEventListener("resize",e)},[]),i}export{s as u};
