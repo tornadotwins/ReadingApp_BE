ad11682be1aeba1f151565f210af0f892b93e7c1
livewater009 <fengbeststar@gmail.com>
2025-05-24 18:47:49 -0500

    Fix reference language issue

---
+++ frontend/assets/index-NAP0fZxs.js
@@ -0,0 +1,1 @@
+import{b as a,j as t}from"./index-CRP-wFOv.js";import{S as i,g as x,a9 as m}from"./App-pXEb52xN.js";import{T as l}from"./index-b-BRRS9h.js";function u({label:o,color:e="default",onClick:r}){const{theme:s}=a.useContext(i),{altTextColor:n}=x(s);return t.jsx(m,{color:e,onClick:r,children:t.jsx(l,{color:n,children:o})})}export{u as L};
