d7bfa5946c9a6645393eb0bf77bce343921196be
livewater009 <fengbeststar@gmail.com>
2025-05-24 18:31:19 -0500

    Fix Reference language

---
+++ frontend/assets/index-CUAImvvL.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-CRP-wFOv.js";import{b5 as t,b6 as o}from"./App-CQHI11oL.js";function d(){return r.jsx(t,{children:r.jsx(o,{width:50,height:50,color:"white"})})}export{d as L};
