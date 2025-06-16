83747e1f3b5ca896509e9792e20a5db1caf5abb7
livewater009 <fengbeststar@gmail.com>
2025-06-15 22:14:23 -0500

    Update: db url

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,6 @@
-const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://localhost:27017';
+const url =
+  'mongodb+srv://mfchb094:GGGQbosUP2pT7Ln6@readningapp.5sllf.mongodb.net/readingapp?retryWrites=true&w=majority&appName=ReadningApp';
 
 module.exports = {
   url,
